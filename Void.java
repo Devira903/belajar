fb290874306b5704895c98a6311df5eb8e24cc79
Devira903 <73265698+Devira903@users.noreply.github.com>
2021-02-28 09:09:55 +0700

    Add files via upload

---
+++ Void.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgvoid;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MOKLET-1
+ */
+public class Void {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String Nama, TempatTanggalLahir, NomorNISN, Alamat, JenisKelamin;
+        
+    }
+    void perkenalan(){
+        System.out.println("Data NISN");
+    }
+    void user(){
+        Scanner io= new Scanner(System.in);
+        
+        System.out.println("");
+        System.out.println("=============");
+        System.out.println("Nama: ");
+        Nama: io.nextLine();
+        System.out.println("Tempat Tanggal Lahir: ");
+        TempatTanggalLahir: io.nextLine();
+        System.out.println("Nomor NISN: ");
+        NomorNISN: io.nextLine();
+        System.out.println("Alamat: ");
+        Alamat: io.nextLine();
+        System.out.println("JenisKelamin");
+        JenisKelamin: io.nextLine();
+        
+        System.out.println("=============");
+        
+    }
+    
+}
